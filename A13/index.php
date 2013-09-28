f5bed5ffe8ebb2f831e1c6ee35b76288339808c1
Marek <marekl@hawaii.edu>
2013-09-28 01:25:17 -1000

    Site for A13

---
+++ A13/index.php
@@ -0,0 +1,30 @@
+<?php
+if(isset($_POST['comment'])){
+		if($_POST['comment'] != null){
+		$writeHandle = fopen("sample.txt","a+");
+		$newComment = $_POST['comment'];
+		fwrite($writeHandle, $newComment);
+		fwrite($writeHandle, "</br>");
+		fclose($writeHandle);
+	}
+}
+echo "Previous comments:</br>";
+$readHandle = fopen("sample.txt",'r');
+while ($line = fgets($readHandle)){
+	 echo "$line</br>";
+}
+fclose($readHandle);
+?>
+
+<!DOCTYPE html>
+<head>
+	<title>Marek Lopez A13 PHP Demo</title>
+</head>
+<body>
+<form name='addComment' action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
+	 Enter a comment:<input type='text' name='comment' autofocus="autofocus"/>
+	 <input type='submit' value='Submit comment' name='submit_button'/>
+</form>
+</body>
+</html>
+
